ab3a43039b6cd5bf027a68e3781c0c30313e39e1
Josefine klasson <jossanwowar93@gmail.com>
2025-02-12 13:54:03 +0100

    added no token chat

--- app/Http/Controllers/ChatbotController.php
+++ app/Http/Controllers/ChatbotController.php
@@ -1,15 +1,20 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Http;
 
 use Illuminate\Http\Request;
 
 class ChatbotController extends Controller
 {
-    //
-   $responseData = Http::post('http://localhost:11434/api/generate', [
-    'model' => 'mistral',
-    'prompt' => $request->message,
-    'stream' => false
-    ])
+    public function chat(Request $request){
+
+        $responseData = Http::post('http://localhost:11434/api/generate', [
+        'model' => 'mistral',
+        'prompt' => $request->message,
+        'stream' => false
+       ]);
+       return response()->json($responseData->json());
+       }
+
 }
